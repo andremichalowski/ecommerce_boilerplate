2df2a70b73c183ffa827c0693b9e447f05639cea
amichalo421 <andre.michalowski@gmail.com>
2022-07-08 14:02:28 -0400

    JWT Signin + Signout

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,15 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken"); // to generate signed token
+const expressJwt = require("express-jwt"); // for authorization check
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
-  console.log("req.body", req.body);
+  // console.log("req.body", req.body);
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: errorHandler(err),
+        error: errorHandler(err),
       });
     }
     user.salt = undefined;
@@ -16,6 +18,40 @@ exports.signup = (req, res) => {
       user,
     });
   });
+};
 
-  console.log(`res sent controllers/user.js at ${Date().toLocaleString()}`);
+exports.signin = (req, res) => {
+  console.log("you got this far");
+  // find the user based on email
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    console.log("Finding user based on email");
+    if (err || !user) {
+      console.log("err || !user");
+      return res.status(400).json({
+        error: "User with that email does not exist. Please signup",
+      });
+    }
+    // if user is found make sure the email and password match
+    // create authenticate method in user model
+    if (!user.authenticate(password)) {
+      console.log('authenticated didn"t work');
+      return res.status(401).json({
+        error: "Email and password dont match",
+      });
+    }
+    // generate a signed token with user id and secret
+    console.log(process.env);
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    // persist the token as 't' in cookie with expiry date
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    // return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, email, name, role } });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("t");
+  res.json({ message: "Signout success" });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { userSignupValidator } = require("../validator/index.js");
 
-const { signup } = require("../controllers/user.js");
-const { test } = require("../controllers/testCont.js");
+const { signup, signin, signout } = require("../controllers/user");
+const { userSignupValidator } = require("../validator");
 
-router.get("/test", test);
 router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get('/signout', signout)
 
 module.exports = router;
