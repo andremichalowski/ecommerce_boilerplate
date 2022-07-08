3bb08300407884e6ac5ebe93be4636bc6575c8b9
amichalo421 <andre.michalowski@gmail.com>
2022-07-08 19:37:12 -0400

    Refactor user to auth

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 
 require("dotenv").config(); //load env var
-const userRoutes = require("./routes/user.js");
+const authRoutes = require("./routes/auth.js");
 
 // app
 const app = express();
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 
 // routes middleware
-app.use("/api", userRoutes);
+app.use("/api", authRoutes);
 
 const port = process.env.PORT || 8000;
 
