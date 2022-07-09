0d846fb3293d5bc43aa0b0f6c34956b89f987607
amichalo421 <andre.michalowski@gmail.com>
2022-07-08 23:19:47 -0400

    Auth and Admin middlewares

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const { requireSignin } = require("../controllers/auth");
 const router = express.Router();
 
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get("/secret/:userId", requireSignin, isAdmin, (req, res) => {
   res.json({
-    user: req.profile
+    user: req.profile,
   });
 });
 
-router.param('userId', userById);
+router.param("userId", userById);
 
 module.exports = router;
